0576015d32dacfaa2219ba15916d65a7139dee20
dima41095 <dima41095@gmail.com>
2026-05-11 17:49:31 +0300

    feat: добавил цикл для повторных вычислений

--- calculator.cpp
+++ calculator.cpp
@@ -8,44 +8,57 @@ int main() {
     
     double num1, num2;
     char operation;
+    char choice;  // для выбора продолжения работы
     
     cout << "===== ПРОСТОЙ КАЛЬКУЛЯТОР =====" << endl;
-    cout << "Введите первое число: ";
-    cin >> num1;
+    cout << "Программа поддерживает повторные вычисления!" << endl << endl;
     
-    cout << "Введите операцию (+, -, *, /, ^): ";
-    cin >> operation;
-    cout << "Введите второе число: ";
-    cin >> num2;
+    do {
+        // Ввод данных
+        cout << "Введите первое число: ";
+        cin >> num1;
+        
+        cout << "Введите операцию (+, -, *, /, ^): ";
+        cin >> operation;
+        
+        cout << "Введите второе число: ";
+        cin >> num2;
+        
+        // Вывод результата
+        cout << "\nРезультат:" << endl;
+        
+        switch(operation) {
+            case '+':
+                cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+                break;
+            case '-':
+                cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+                break;
+            case '*':
+                cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+                break;
+            case '/':
+                if(num2 != 0) {
+                    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+                } else {
+                    cout << "ОШИБКА: Деление на ноль невозможно!" << endl;
+                }
+                break;
+            case '^':
+                cout << num1 << " ^ " << num2 << " = " << pow(num1, num2) << endl;
+                break;
+            default:
+                cout << "ОШИБКА: Операция '" << operation << "' не поддерживается!" << endl;
+                cout << "Доступные операции: +, -, *, /, ^" << endl;
+        }
+        
+        // Спрашиваем пользователя о продолжении
+        cout << "\nВыполнить ещё одно вычисление? (y/n): ";
+        cin >> choice;
+        cout << endl;
+        
+    } while(choice == 'y' || choice == 'Y');
     
-    cout << "\nРезультат:" << endl;
-    
-    switch(operation) {
-        case '+'
-            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
-            break;
-        case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
-            break;
-        case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
-            break;
-        case '/':
-            if(num2 != 0) {
-                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
-            } else {
-                cout << "ОШИБКА: Деление на ноль невозможно!" << endl;
-            }
-            break;
-        case '^':  // возведение в степень
-            cout << num1 << " ^ " << num2 << " = " << pow(num1, num2) << endl;
-            break;
-        default:
-            cout << "ОШИБКА: Операция '" << operation << "' не поддерживается!" << endl;
-            cout << "Доступные операции: +, -, *, /, ^" << endl;
-    }
-    
-    cout << "\nСпасибо за использование калькулятора!" << endl;
-    cout << "Программа завершена" << endl;
+    cout << "Спасибо за использование калькулятора!" << endl;
     return 0;
 }
